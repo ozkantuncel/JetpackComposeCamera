ddc06ad4278dba14b4cafcd66e91b36c80237ad8
Ozkan Tuncel <ozkantuncel2016@gmail.com>
2023-07-12 21:24:40 +0300

    Updated ImageDaoRepositoryImpl and build.gradle

--- app/src/main/java/com/example/jetpackcomposecamera/data/repository/ImageDaoRepositoryImpl.kt
+++ app/src/main/java/com/example/jetpackcomposecamera/data/repository/ImageDaoRepositoryImpl.kt
@@ -1,44 +1,29 @@
 package com.example.jetpackcomposecamera.data.repository
 
 import android.app.Application
-import androidx.lifecycle.MutableLiveData
 import com.example.jetpackcomposecamera.data.local.database.ImageDatabase
 import com.example.jetpackcomposecamera.data.model.ImageModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-class ImageDaoRepositoryImpl(var application: Application):ImageDaoRepository {
+class ImageDaoRepositoryImpl(private var application: Application):ImageDaoRepository {
 
-    var getImageList: MutableLiveData<List<ImageModel>> = MutableLiveData()
+    private var database: ImageDatabase = ImageDatabase.getDatabase(application)
 
-    var database: ImageDatabase = ImageDatabase.getDatabase(application)
 
     override suspend fun getImagesByNameAsc(): List<ImageModel> {
         return try {
-            val myList = database.imageDao().getImagesByNameAsc()
-            myList
-        }catch (e: Exception){
+            val imageList = database.imageDao().getImagesByNameAsc()
+            imageList
+        }catch (e:Exception){
             TODO()
         }
     }
 
     override suspend fun deleteImage(imageModel: ImageModel) {
-        withContext(Dispatchers.IO){
-            database.imageDao().deleteImage(image = imageModel)
-        }
+        database.imageDao().deleteImage(image = imageModel)
     }
 
     override suspend fun insertImage(imageModel: ImageModel) {
-        withContext(Dispatchers.IO){
-            database.imageDao().insertImage(image =imageModel )
-        }
-    }
+        database.imageDao().insertImage(image =imageModel )
 
-    private fun getImages(){
-        CoroutineScope(Dispatchers.Main).launch {
-            getImageList.value = database.imageDao().getImagesByNameAsc()
-        }
     }
 }
