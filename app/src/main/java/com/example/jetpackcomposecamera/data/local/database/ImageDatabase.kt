3c66cc30f1033b0b0f2a85911cb80c076ab7b3b6
Ozkan Tuncel <ozkantuncel2016@gmail.com>
2023-07-18 17:08:27 +0300

    removed Room database builder

--- app/src/main/java/com/example/jetpackcomposecamera/data/local/database/ImageDatabase.kt
+++ app/src/main/java/com/example/jetpackcomposecamera/data/local/database/ImageDatabase.kt
@@ -1,13 +1,9 @@
 package com.example.jetpackcomposecamera.data.local.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.jetpackcomposecamera.data.local.dao.ImageDao
-import com.example.jetpackcomposecamera.util.Constant.DATABASE_NAME
 import com.example.jetpackcomposecamera.data.model.ImageModel
-
 
 
 // entities: Veritabanında bulunan tablolar
@@ -15,29 +11,7 @@ import com.example.jetpackcomposecamera.data.model.ImageModel
 // exportSchema: Veritabanı şemasının dışa aktarılıp aktarılmayacağını belirtir
 @Database(entities = [ImageModel::class], version = 1, exportSchema = false)
 abstract class ImageDatabase : RoomDatabase() {
-
     // ImageDao arayüzünü döndüren soyut bir metot
     // Bu metot, veritabanı işlemlerini gerçekleştirmek için kullanılır
     abstract fun imageDao(): ImageDao
-
-    companion object {
-        // Veritabanı örneğini tutan bir statik bir değişken
-        // @Volatile anahtar kelimesi, bu değişkenin birden fazla iş parçacığı tarafından erişilebileceğini belirtir
-        @Volatile
-        private var INSTANCE: ImageDatabase? = null
-
-        // Veritabanı döndüren bir fonksiyon
-        // Eğer veritabanı daha önce oluşturulmamışsa, bu fonksiyon yeni bir veritabanı oluşturur
-        fun getDatabase(context: Context): ImageDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    ImageDatabase::class.java,
-                    DATABASE_NAME
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
