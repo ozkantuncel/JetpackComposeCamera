f73cae68073ff0c6454c95b29fda567acce44dc1
Ozkan Tuncel <ozkantuncel2016@gmail.com>
2023-07-19 22:09:43 +0300

    refactor move applicationComponent to companion object

--- app/src/main/java/com/example/jetpackcomposecamera/di/JetpackComposeCameraApp.kt
+++ app/src/main/java/com/example/jetpackcomposecamera/di/JetpackComposeCameraApp.kt
@@ -3,16 +3,17 @@ package com.example.jetpackcomposecamera.di
 import android.app.Application
 
 
-// @HiltAndroidApp anotasyonu, uygulamanızın Application sınıfının Hilt tarafından yönetilmesini sağlar.
 // AndroidManifest.xml de uygulamanızın Application sınıfının adını belirtmeniz gerekir
-// Bu anotasyon, Hilt'in uygulamanızın tüm bağımlılıkları için otomatik olarak bir bağımlılık yöneticisi oluşturmasını sağlar kısaca.
-
 class JetpackComposeCameraApp : Application() {
-
-    lateinit var applicationComponent: ApplicationComponent
+    // Companion object içinde bir ApplicationComponent nesnesi tanımlı.
+    companion object {
+        lateinit var applicationComponent: ApplicationComponent
+    }
 
     override fun onCreate() {
         super.onCreate()
+        // ApplicationComponent nesnesini DaggerApplicationComponent builder'ını kullanarak oluşturuyor.
+        // AppModule modülünü de dahil ediyor ve context olarak uygulama context'ini veriyoruz.
         applicationComponent =
             DaggerApplicationComponent.builder().appModule(AppModule(context = this)).build()
     }
