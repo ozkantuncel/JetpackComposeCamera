7cb95847d3530a2d47f85aef5947660758b9f038
Ozkan Tuncel <ozkantuncel2016@gmail.com>
2023-07-19 22:01:59 +0300

    refactor ApplicationComponent to use inject methods

--- app/src/main/java/com/example/jetpackcomposecamera/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/jetpackcomposecamera/di/ApplicationComponent.kt
@@ -1,14 +1,16 @@
 package com.example.jetpackcomposecamera.di
 
-import com.example.jetpackcomposecamera.presentation.camera_screen.viewmodel.CameraViewModelFactory
-import com.example.jetpackcomposecamera.presentation.main_screen.viewmodel.MainScreenViewModelFactory
+import com.example.jetpackcomposecamera.presentation.camera_screen.viewmodel.CameraViewModel
+import com.example.jetpackcomposecamera.presentation.main_screen.viewmodel.MainScreenViewModel
 import dagger.Component
 import javax.inject.Singleton
 
 
+// Singleton anotasyonu ile bu component'in uygulama boyunca tek bir instance'ı olacağını belirtiyor.
+// Component anotasyonu ile bu sınıfın bir Dagger component'i olduğunu belirtiyor.
 @Singleton
-@Component(modules = [AppModule::class])
+@Component(modules = [AppModule::class, DataModule::class])
 interface ApplicationComponent {
-    fun mainScreenViewModelFactory():MainScreenViewModelFactory
-    fun cameraViewViewModelFactory(): CameraViewModelFactory
+    fun inject(cameraViewModel:CameraViewModel)
+    fun inject(mainScreenViewModel: MainScreenViewModel)
 }
