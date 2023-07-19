0afa8138a931296a62f8328c6d983bf20114fd7f
Ozkan Tuncel <ozkantuncel2016@gmail.com>
2023-07-19 22:12:47 +0300

    refactor use Dagger to inject repository into ViewModel

--- app/src/main/java/com/example/jetpackcomposecamera/presentation/camera_screen/viewmodel/CameraViewModel.kt
+++ app/src/main/java/com/example/jetpackcomposecamera/presentation/camera_screen/viewmodel/CameraViewModel.kt
@@ -4,17 +4,29 @@ package com.example.jetpackcomposecamera.presentation.camera_screen.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetpackcomposecamera.data.model.ImageModel
-import com.example.jetpackcomposecamera.data.repository.ImageDaoRepository
+import com.example.jetpackcomposecamera.data.repository.ImageDaoRepositoryImpl
+import com.example.jetpackcomposecamera.di.JetpackComposeCameraApp
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 // MVVM (Model-View-ViewModel) desenine uygundur
 // ViewModel, Activity, Fragment ve Composable fonksiyonlarla gibi yaşam döngüsüne sahip bileşenlerle veri paylaşımını sağlar.
 
-class CameraViewModel(private val repository: ImageDaoRepository) : ViewModel() {
+class CameraViewModel() : ViewModel() {
+
+    // init bloğu, sınıfın constructor'ı çağrıldığında çalışır.
+    @Inject
+    lateinit var repository: ImageDaoRepositoryImpl
+
+    init {
+        // ApplicationComponent nesnesini kullanarak bu sınıfa bağımlılıkları enjekte ediyoruz.
+        JetpackComposeCameraApp.applicationComponent.inject(this)
+    }
 
     // viewModelScope.launch{} bloğu, bir coroutine scope'u oluşturur ve bu scope içindeki işlemler asenkron olarak çalışır.
     fun insertData(imageModel: ImageModel) {
+        // viewModelScope.launch{} bloğu, bir coroutine scope'u oluşturur ve bu scope içindeki işlemler asenkron olarak çalışır.
         viewModelScope.launch {
             repository.insertImage(imageModel)
         }
--- app/src/main/java/com/example/jetpackcomposecamera/presentation/main_screen/viewmodel/MainScreenViewModel.kt
+++ app/src/main/java/com/example/jetpackcomposecamera/presentation/main_screen/viewmodel/MainScreenViewModel.kt
@@ -6,29 +6,41 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetpackcomposecamera.data.model.ImageModel
-import com.example.jetpackcomposecamera.data.repository.ImageDaoRepository
+import com.example.jetpackcomposecamera.data.repository.ImageDaoRepositoryImpl
+import com.example.jetpackcomposecamera.di.JetpackComposeCameraApp
 import kotlinx.coroutines.launch
 import java.io.File
+import javax.inject.Inject
 
-class MainScreenViewModel  (
-    private val repository: ImageDaoRepository
-):ViewModel(){
+class MainScreenViewModel() : ViewModel() {
+
+    // ImageDaoRepositoryImpl nesnesini enjekte ediyoruz.
+    @Inject
+    lateinit var repository: ImageDaoRepositoryImpl
 
     // LiveData olarak tanımlanan _listImage değişkeni, dışarıdan erişime açık değildir
     // LiveData, veri değişikliklerini otomatik olarak takip eden ve güncelleyen bir yapıdır
-    private var _listImage =  MutableLiveData<List<ImageModel>>()
+    private var _listImage = MutableLiveData<List<ImageModel>>()
+
     // listImage değişkeni, dışarıdan erişilebilir LiveData olarak tanımlı
     val listImage: LiveData<List<ImageModel>> = _listImage
     // İlk olarak fetchData() fonksiyonunu çağırarak verileri getirir
 
-     fun fetchData(){
+    // init bloğu, sınıfın constructor'ı çağrıldığında çalışır.
+    init {
+        JetpackComposeCameraApp.applicationComponent.inject(this)
+    }
+
+    fun fetchData() {
+        // viewModelScope.launch{} bloğu, bir coroutine scope'u oluşturur ve bu scope içindeki işlemler asenkron olarak çalışır.
         viewModelScope.launch {
             val result = repository.getImagesByNameAsc()
             _listImage.value = result
         }
     }
 
-    fun deleteData(imageModel: ImageModel){
+    fun deleteData(imageModel: ImageModel) {
+        // viewModelScope.launch{} bloğu, bir coroutine scope'u oluşturur ve bu scope içindeki işlemler asenkron olarak çalışır.
         viewModelScope.launch {
             repository.deleteImage(imageModel = imageModel)
             fetchData()
