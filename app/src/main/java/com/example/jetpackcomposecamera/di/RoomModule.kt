0fc3916d000864c3f0b98ddd0f8bfc23f249adf5
Ozkan Tuncel <ozkantuncel2016@gmail.com>
2023-07-24 12:37:15 +0300

    added Hilt modules for ImageRepository and Room database setup

---
+++ app/src/main/java/com/example/jetpackcomposecamera/di/RoomModule.kt
@@ -0,0 +1,31 @@
+package com.example.jetpackcomposecamera.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.jetpackcomposecamera.data.local.dao.ImageDao
+import com.example.jetpackcomposecamera.data.local.database.ImageDatabase
+import com.example.jetpackcomposecamera.util.Constant
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object RoomModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): ImageDatabase =
+        Room.databaseBuilder(
+            context,
+            ImageDatabase::class.java,
+            Constant.DATABASE_NAME
+        ).build()
+
+    @Provides
+    @Singleton
+    fun provideImageDataDao(database: ImageDatabase): ImageDao = database.imageDao()
+}
