179d468b57f965b4f8bbe33a10992c1b3d14761a
Ozkan Tuncel <ozkantuncel2016@gmail.com>
2023-07-25 15:26:07 +0300

    added color

--- app/src/main/java/com/example/jetpackcomposecamera/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetpackcomposecamera/presentation/ui/theme/Theme.kt
@@ -22,9 +22,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = ColorApp60,
+    secondary = ColorApp30,
+    tertiary = Color2App10
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
