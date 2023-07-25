37f7f63b3c5b63d7f31d7df8ca60493d3e69ffd4
Ozkan Tuncel <ozkantuncel2016@gmail.com>
2023-07-25 09:15:43 +0300

    updated NavGraph and Screen

--- app/src/main/java/com/example/jetpackcomposecamera/presentation/navigation/Screen.kt
+++ app/src/main/java/com/example/jetpackcomposecamera/presentation/navigation/Screen.kt
@@ -3,4 +3,8 @@ package com.example.jetpackcomposecamera.presentation.navigation
 sealed class Screen(val route: String){
     object MainScreen: Screen(route = "main_screen")
     object CameraView: Screen(route = "camera_screen")
+
+    object LoginScreen: Screen(route = "login_screen")
+
+    object RegisterScreen: Screen(route = "register_screen")
 }
